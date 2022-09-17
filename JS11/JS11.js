b1c65c31081b0e600d0973e857ee816e2f6e73ef
karamguliyev <karamguliyev10@gmail.com>
2022-09-16 23:21:45 -0700

    JS11 Finish

---
+++ JS11/JS11.js
@@ -0,0 +1,10 @@
+// Modify the second term of the expression in order to get true as result.
+console.log(
+  10 < 11,
+  45 == 45,
+  true === true,
+  false === false,
+  1 == true,
+  4 <= 0,
+  4 >= 10,
+);
