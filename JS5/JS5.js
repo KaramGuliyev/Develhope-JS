110b50a7c52d62080a61c61cf34fe3ab31f934a1
karamguliyev <karamguliyev10@gmail.com>
2022-09-16 22:37:54 -0700

    JS5 Finish

---
+++ JS5/JS5.js
@@ -0,0 +1,10 @@
+var isDoorClosed = false
+var isDogOutside = true
+
+if (isDoorClosed == true && isDogOutside == false) {
+    console.log("the door is closed and the dog is inside");
+    } else if (isDoorClosed == false && isDogOutside == true) {
+    console.log("The door is open and the dog is outside");
+    } else console.log("Modify the variables values");
+
+// && is means that if we have one or more conditions it should be correct for all conditions for working, if any condition gives false it proceeds to next condition. 
