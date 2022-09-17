59762629d01f2bb2531b1778dc39a3bea07707a9
karamguliyev <karamguliyev10@gmail.com>
2022-09-16 23:16:52 -0700

    JS10 Finish

---
+++ JS10/JS10.js
@@ -0,0 +1,9 @@
+// Explain the difference between the double and the triple equals operator.
+
+const firstYearCompleted = true;
+const yearsCompleted = 1;
+
+console.log(yearsCompleted == firstYearCompleted);
+console.log(yearsCompleted === firstYearCompleted);
+
+// As I said before == is only value comparison, but === is compares types too beside to values
--- JS9/JS9.js
+++ JS9/JS9.js
@@ -12,8 +12,8 @@ const yearsCompleted = 1;
 console.log(name == lastname ); // False because Strings should be same including lower and upper cases
 console.log(age <= average); // Age is greater or equals to average, so it's true
 console.log(firstYearCompleted == lastname); // Boolean value can not be equal to string even if it's "true"
-console.log(yearsCompleted == firstYearCompleted); // This is true, the reason of behind this in coding 1 means true when this line starts working JS reads 1 as a true
-console.log(firstYearCompleted === yearsCompleted); // This is false because of this is not same way, it starts with boolean value and in second part it reads as a number not a true boolean value
+console.log(yearsCompleted == firstYearCompleted); // This is true, the reason of behind this I think in coding 1 means true when this line starts working JS reads 1 as a true
+console.log(firstYearCompleted === yearsCompleted); // This is false because of === means exact values, due to it's not exact same value it return as false
 console.log(examsCompleted < age); // Exam count is lower than age, it's true
 console.log(isGraduated > yearsCompleted); // Boolean value can not be greater, it's false
 
