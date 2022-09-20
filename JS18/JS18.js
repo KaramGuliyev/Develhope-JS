d1faa8de16600d1ba796f55f71058eca7bdfa34e
karamguliyev <karamguliyev10@gmail.com>
2022-09-19 23:29:52 -0700

    JS18 Finished

---
+++ JS18/JS18.js
@@ -0,0 +1,9 @@
+// Rewrite the code below, by using the ternary operator.
+
+let number = 0;
+
+// if (number > 0) {
+// console.log("The number is positive");
+// } else console.log("The number is negative");
+
+console.log(number > 0 ? "The number is positive" : "The number is negative"); 
