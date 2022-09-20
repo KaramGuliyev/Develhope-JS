bfad1cff9d0d8a223d81570b7b8182bdd05d6437
karamguliyev <karamguliyev10@gmail.com>
2022-09-19 22:58:04 -0700

    JS16 Finish

---
+++ JS16/JS16.js
@@ -0,0 +1,23 @@
+// Create two boolean variables called isSunnyDay and isHomeworkCompleted. 
+// Assign them a value. Use the Ternary Operator in order to print Jesse can go out to play, 
+// if both variables are true. If one of them or both are false print Jesse stays home.
+
+var isSunnyDay = true;
+var isHomeworkCompleted = false;
+
+function strictParents3000() {
+    if (isSunnyDay && isHomeworkCompleted) {
+        console.log("You can go to play with your friends but be at home before 6");
+    }
+    else if (isSunnyDay && !isHomeworkCompleted ) {
+        console.log("You will fail if you continue like this!");
+    }
+    else if ( !isSunnyDay && !isHomeworkCompleted) {
+        console.log("You should stay at home and do your homework! Look at this weather what you want to do? Be a sick and skip school?");
+    }
+    else if (!isSunnyDay && isHomeworkCompleted) {
+        console.log("Its raining, do your next weeks homework.");
+    }
+}
+
+strictParents3000();
